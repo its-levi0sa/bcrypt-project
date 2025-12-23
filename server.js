1f248bf0dd5a8d241048b83d51fb29d3ae2a7844
hedwig <150428098+its-levi0sa@users.noreply.github.com>
2025-12-23 13:11:15 +0800

    Update

--- server.js
+++ server.js
@@ -10,17 +10,16 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res);
   });
 });
-
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
 
 //END_ASYNC
 
